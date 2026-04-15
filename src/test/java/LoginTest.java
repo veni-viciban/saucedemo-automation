9c356748f4c4e4e4c3edbc6f62834ea53432d137
veni-viciban <vraya.nababan@gmail.com>
2026-04-15 21:01:42 +0700

    LoginTest.java

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class LoginTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    @Test
+    public void loginStandardUser() {
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+
+        Assert.assertTrue(driver.getCurrentUrl().contains("inventory"));
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
