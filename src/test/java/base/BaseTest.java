caf9bfca5b5a7d6d0f207233bba2717e7690a25c
veni-viciban <vraya.nababan@gmail.com>
2026-04-15 21:03:20 +0700

    BaseTest.java

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,22 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) driver.quit();
+    }
+}
